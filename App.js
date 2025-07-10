c7a4674472930e51bd70f0b1ef75dfc400ced3af
paveltorres <paveltorres2020@gmail.com>
2025-07-10 16:07:07 -0500

    personajes

---
+++ App.js
@@ -0,0 +1,26 @@
+
+import { SafeAreaView, StyleSheet } from 'react-native';
+import RickAndMortyCharacters from './lib/reckieMartinService';
+
+export default function App() {
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <RickAndMortyCharacters />
+    </SafeAreaView>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 60,
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  titulo: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
