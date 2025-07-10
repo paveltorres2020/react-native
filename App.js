28bd5483ba3417e36693de59600d490ca2dbee17
paveltorres <paveltorres2020@gmail.com>
2025-07-10 18:37:50 -0500

    image svg

--- App.js
+++ App.js
@@ -1,31 +1,21 @@
 
-import { SafeAreaView, StyleSheet } from 'react-native';
-import RickAndMortyCharacters from './lib/reckieMartinService';
-
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Logo from './assets/logo.svg';
 
 export default function App() {
-
   return (
-      <SafeAreaView style={styles.container}>
-       <RickAndMortyCharacters />
-
-     </SafeAreaView> 
-    
-    
+    <View style={styles.container}>
+      <Logo width={500} height={500} />
+    </View>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 60,
+    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f5f5f5',
-  },
-  titulo: {
-    fontSize: 24,
-    fontWeight: 'bold',
+    backgroundColor: '#fff',
   },
 });
- 
